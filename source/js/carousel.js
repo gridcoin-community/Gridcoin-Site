d4277b82734a3b9cfeabf89cced0e50d45c58722
grcgrc <grctestemail@gmail.com>
2016-05-17 14:43:11 +0100

    Quick changes
    Carousel was previously removed, no point in their files remaining.

--- source/js/carousel.js
+++
@@ -1,8 +0,0 @@
-// When the DOM is ready, run this function
-$(document).ready(function () {
-    //Set the carousel options
-    $('#quote-carousel').carousel({
-        pause: true,
-        interval: 40000,
-    });
-});
