219c87875e9895057291885617b3efba2ec73d2b
RoboticMind <30132912+RoboticMind@users.noreply.github.com>
2020-09-05 12:59:23 -0400

    Add Direct Download Links For Windows Linux ones could be added in the future Doesn't show direct links if missing or loading

--- assets/js/fetch-release.js
+++ assets/js/fetch-release.js
@@ -5,7 +5,24 @@ function getLatestRelease() {
         crossDomain: true,
         dataType: 'json',
         success: function(data) { 
-            $('#wallet-version').text('Current Wallet Version: ' + data['name']); 
+            $('#wallet-version').text('Current Wallet Version: ' + data.name); 
+            
+            String.prototype.endsWith = "".endsWith || function(s){return !this.split(s).pop();} //IE11 support
+
+            data.assets.forEach(function(assetFile) {
+
+                if (assetFile.name.endsWith("win64-setup.exe")){
+
+                    $('#64-bit-windows').attr('href', assetFile.browser_download_url).show()
+
+                } 
+
+                if (assetFile.name.endsWith("win32-setup.exe")){
+
+                    $('#32-bit-windows').attr('href', assetFile.browser_download_url).show()
+
+                }
+            });
         }
     });
 }
