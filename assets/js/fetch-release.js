64b0717b019a4b45c3adbaa6d509e81f5ccf1496
barton26 <barton2526@gmail.com>
2021-04-08 17:54:55 -0400

    Formatting fixes from jslint (#293)
    adding/removing whitespace
    adding ; where missing

--- assets/js/fetch-release.js
+++ assets/js/fetch-release.js
@@ -1,49 +1,46 @@
 function getLatestRelease() {
     $.ajax({
         type: 'GET',
-        url:'https://api.github.com/repos/gridcoin-community/Gridcoin-Research/releases/latest',
+        url: 'https://api.github.com/repos/gridcoin-community/Gridcoin-Research/releases/latest',
         crossDomain: true,
         dataType: 'json',
-        success: function(data) { 
-            $('#wallet-version').text('Current Wallet Version: ' + data.name); 
+        success: function (data) {
+            $('#wallet-version').text('Current Wallet Version: ' + data.name);
             
-            String.prototype.includes = String.prototype.includes || function(val, start){'use strict';return this.indexOf(val, start) !== -1;};
+            String.prototype.includes = String.prototype.includes || function (val, start) {'use strict'; return this.indexOf(val, start) !== -1; };
             //IE11 support
 
             var has64BitHotfix = false;
             var has32BitHotfix = false;
 
-            data.assets.forEach(function(assetFile) {
+            data.assets.forEach(function (assetFile) {
                 
                 const name = assetFile.name;
                 const downloadURL = assetFile.browser_download_url;
 
-                if (name.includes(".SHA256")){
+                if (name.includes(".SHA256")) {
                     //ignore the checksums (note can't use "continue" so else if statements are used)
                 }
-
-                else if (name.includes("win64") && name.includes("hotfix")){
+                else if (name.includes("win64") && name.includes("hotfix")) {
 
                     has64BitHotfix = true;
                     $('#64-bit-windows').attr('href', downloadURL).show();
 
-                } 
-                else if (name.includes("win64") && !has64BitHotfix){ 
+                }
+                else if (name.includes("win64") && !has64BitHotfix) {
 
-                    $('#64-bit-windows').attr('href', downloadURL).show()
+                    $('#64-bit-windows').attr('href', downloadURL).show();
 
-                } 
-
-
-                else if (name.includes("win32") && name.includes("hotfix")){
+                }
+                else if (name.includes("win32") && name.includes("hotfix")) {
                     
                     has32BitHotfix = true;
                     $('#32-bit-windows').attr('href', downloadURL).show();
                     
                 }
-                else if (name.includes("win32") && !has32BitHotfix){
+                else if (name.includes("win32") && !has32BitHotfix) {
 
-                    $('#32-bit-windows').attr('href', downloadURL).show()
+                    $('#32-bit-windows').attr('href', downloadURL).show();
 
                 }
             });
@@ -51,9 +48,9 @@ function getLatestRelease() {
     });
 }
 
- $(document).ready(function() {
+ $(document).ready(function () {
     getLatestRelease();
     $(function () {
-        $('[data-toggle="tooltip"]').tooltip() //Enabled bootstrap tooltips.
+        $('[data-toggle="tooltip"]').tooltip(); //Enabled bootstrap tooltips.
     })
-}); 
+});
