4b8745e75585f23ed08278ddc2ed87a342085b95
RoboticMind <30132912+RoboticMind@users.noreply.github.com>
2021-12-28 11:20:06 -0500

    Remove jQuery no longer needed with bootstrap 5 and other uses are easy to remove

--- assets/js/fetch-release.js
+++ assets/js/fetch-release.js
@@ -1,56 +1,60 @@
-function getLatestRelease() {
-    $.ajax({
-        type: 'GET',
-        url: 'https://api.github.com/repos/gridcoin-community/Gridcoin-Research/releases/latest',
-        crossDomain: true,
-        dataType: 'json',
-        success: function (data) {
-            $('#wallet-version').text('Current Wallet Version: ' + data.name);
-            
-            String.prototype.includes = String.prototype.includes || function (val, start) {'use strict'; return this.indexOf(val, start) !== -1; };
-            //IE11 support
+function updateDownloads(data) {
+    document.getElementById("wallet-version").textContent  = 'Current Wallet Version: ' + data.name;
+    
+    String.prototype.includes = String.prototype.includes || function (val, start) {'use strict'; return this.indexOf(val, start) !== -1; };
+    //IE11 support
 
-            var has64BitHotfix = false;
-            var has32BitHotfix = false;
+    var has64BitHotfix = false;
+    var has32BitHotfix = false;
 
-            data.assets.forEach(function (assetFile) {
-                
-                const name = assetFile.name;
-                const downloadURL = assetFile.browser_download_url;
+    const windows64el = document.getElementById("64-bit-windows")
+    const windows32el = document.getElementById("32-bit-windows")
 
-                if (name.includes(".SHA256")) {
-                    //ignore the checksums (note can't use "continue" so else if statements are used)
-                }
-                else if (name.includes("win64") && name.includes("hotfix")) {
 
-                    has64BitHotfix = true;
-                    $('#64-bit-windows').attr('href', downloadURL).show();
+    data.assets.forEach(function (assetFile) {
+        
+        const name = assetFile.name;
+        const downloadURL = assetFile.browser_download_url;
 
-                }
-                else if (name.includes("win64") && !has64BitHotfix) {
-
-                    $('#64-bit-windows').attr('href', downloadURL).show();
-
-                }
-                else if (name.includes("win32") && name.includes("hotfix")) {
-                    
-                    has32BitHotfix = true;
-                    $('#32-bit-windows').attr('href', downloadURL).show();
-                    
-                }
-                else if (name.includes("win32") && !has32BitHotfix) {
-
-                    $('#32-bit-windows').attr('href', downloadURL).show();
-
-                }
-            });
+        if (name.includes(".SHA256")) {
+            //ignore the checksums (note can't use "continue" so else if statements are used)
         }
-    });
+        else if (name.includes("win64") && name.includes("hotfix")) {
+
+            has64BitHotfix = true;
+            windows64el.href = downloadURL;
+            windows64el.style.display=""; //show the button
+
+        }
+        else if (name.includes("win64") && !has64BitHotfix) {
+            
+            windows64el.href = downloadURL;
+            windows64el.style.display = ""; 
+
+        }
+        else if (name.includes("win32") && name.includes("hotfix")) {
+            
+            has32BitHotfix = true;
+            windows32el.href = downloadURL;
+            windows32el.style.display="";
+            
+        }
+        else if (name.includes("win32") && !has32BitHotfix) {
+
+            windows32el.href = downloadURL;
+            windows32el.style.display="";
+        }
+    })
 }
 
- $(document).ready(function () {
-    getLatestRelease();
-    $(function () {
-        $('[data-bs-toggle="tooltip"]').tooltip(); //Enabled bootstrap tooltips.
-    })
-});
+if (document.getElementById("wallet-version")){ //only get version data on pages where version number and download links are needed
+    fetch("https://api.github.com/repos/gridcoin-community/Gridcoin-Research/releases/latest")
+        .then(response => response.json())
+        .then(updateDownloads)
+}
+
+//Enable bootstrap tooltips.
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
